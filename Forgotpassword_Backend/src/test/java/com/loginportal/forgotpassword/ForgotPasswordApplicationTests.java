e632a2329338ec884ca69aa1367ce4bf6d83857c
srujitha1 <52405021+srujitha1@users.noreply.github.com>
2019-09-05 13:05:01 +0530

    Update ForgotPasswordApplicationTests.java

--- Forgotpassword_Backend/src/test/java/com/loginportal/forgotpassword/ForgotPasswordApplicationTests.java
+++ Forgotpassword_Backend/src/test/java/com/loginportal/forgotpassword/ForgotPasswordApplicationTests.java
@@ -1,16 +1,1 @@
-package com.loginportal.forgotpassword;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ForgotPasswordApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
